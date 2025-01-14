7c14ed740dd7a19988f7d667bb6fa3aa7d10f468
Frode Austvik <edorfaus@xepher.net>
2025-01-14 04:26:28 +0100

    Add a file with macros to convert a binary number to BCD and decimal
    The main algorithm used is Double Dabble, with some optimizations
    already included. Converting from packed BCD to ASCII afterwards can be
    done in several ways, with different tradeoffs, so code for a few
    variants of that is included but not automatically used.

---
+++ dbl-dabble.asm
@@ -0,0 +1,211 @@
+; This file implements the double-dabble algorithm for turning binary
+; numbers into binary coded decimal (BCD), plus some algorithms for
+; unpacking the resulting BCD into ASCII digits.
+;
+; This code is primarily targeted towards high speed rather than low
+; size, but still tries to avoid wasting space for no good reason. Thus,
+; we unroll the loops, but don't automatically use the fastest bcd2ascii
+; code, because its rather small cycle gain has a pretty big data cost.
+
+; Example usage, given appropriate variables BinaryNum and AsciiOut:
+; BinNumToAscii:
+;   mBinToBcd BinaryNum, AsciiOut
+;   mBcdToAscii AsciiOut
+;   rts
+; ...
+; jsr BinNumToAscii
+
+; This macro is the main double-dabble algorithm, converting the binary
+; number in bcdInput to packed BCD in half of bcdOutput. Only half of
+; bcdOutput is used, on the assumption that you'll afterwards expand it
+; into ASCII digits using one of the macros for that.
+; Clobbers: A, X, bcdInput
+.macro mBinToBcd bcdInput, bcdOutput
+	.assert .sizeof(bcdInput) > 0, error, "invalid bcdInput"
+	.assert .sizeof(bcdOutput) > 0, error, "invalid bcdOutput"
+
+	.local @bcdScratchSize, @bcdScratch
+	@bcdScratchSize = (.sizeof(bcdOutput) + 1) / 2
+	; The scratch space is stored big-endian, because that allows the
+	; conversion to ASCII to happen in-place without temporary storage.
+	@bcdScratch = bcdOutput + @bcdScratchSize - 1
+
+	lda #0
+	.repeat @bcdScratchSize-1, i
+		sta @bcdScratch-(i+1)
+	.endrepeat
+
+	; First 3 shifts don't need an add-check, since >= 5 needs 3 bits.
+	.repeat 3
+		asl bcdInput+0
+		.repeat .sizeof(bcdInput)-1, i
+			rol bcdInput+1+i
+		.endrepeat
+		rol a
+	.endrepeat
+	sta @bcdScratch-0
+
+	.repeat 8 * .sizeof(bcdInput) - 3
+		asl bcdInput+0
+		.repeat .sizeof(bcdInput)-1, i
+			rol bcdInput+1+i
+		.endrepeat
+
+		.repeat @bcdScratchSize, i
+			ldx @bcdScratch-i
+			lda bcdAdd3Table, x
+			rol a
+			sta @bcdScratch-i
+		.endrepeat
+	.endrepeat
+.endmacro
+
+; Expands the packed BCD into ASCII digits in a straightforward way.
+; Clobbers: A
+.macro mBcdToAscii bcdOutput
+	.local @bcdScratchSize, @bcdScratch
+	@bcdScratchSize = (.sizeof(bcdOutput) + 1) / 2
+	@bcdScratch = bcdOutput + @bcdScratchSize - 1
+
+	.repeat .sizeof(bcdOutput) / 2, i
+		lda @bcdScratch-i
+		and #$0F
+		ora #'0'
+		sta bcdOutput+.sizeof(bcdOutput)-1-(i*2)
+
+		lda @bcdScratch-i
+		lsr a
+		lsr a
+		lsr a
+		lsr a
+		ora #'0'
+		sta bcdOutput+.sizeof(bcdOutput)-1-(i*2)-1
+	.endrepeat
+.endmacro
+
+; Expands the packed BCD into ASCII digits using the MMC5's multiplier.
+; Clobbers: A, MMC5 multiplier regs
+.macro mBcdToAsciiMMC5 bcdOutput
+	.local @bcdScratchSize, @bcdScratch, @mmc5_MultA, @mmc5_MultB
+	@bcdScratchSize = (.sizeof(bcdOutput) + 1) / 2
+	@bcdScratch = bcdOutput + @bcdScratchSize - 1
+	@mmc5_MultA = $5205
+	@mmc5_MultB = $5206
+
+	lda #$10
+	sta @mmc5_MultA
+
+	.repeat .sizeof(bcdOutput) / 2, i
+		lda @bcdScratch-i
+		sta @mmc5_MultB
+
+		and #$0F
+		ora #'0'
+		sta bcdOutput+.sizeof(bcdOutput)-1-(i*2)
+
+		lda @mmc5_MultB
+		ora #'0'
+		sta bcdOutput+.sizeof(bcdOutput)-1-(i*2)-1
+	.endrepeat
+.endmacro
+
+; Define the table if not already done. Used internally by the macros
+; that need this table, which are rather data-heavy for little gain.
+.macro _mSetup_BcdToAsciiTableHi
+	.ifndef bcdAsciiTableHi
+		.pushseg
+		.segment "LIBDATA"
+		.align $100
+		bcdAsciiTableHi:
+			.repeat 10, i
+				.res 16, '0'+i
+			.endrepeat
+			; Crossing a page boundary will not cause failures, but
+			; will cause the code to run slower.
+			@ok = (.hibyte(*) = .hibyte(bcdAsciiTableHi))
+			.assert @ok, warning, "bcdAsciiTableHi crosses pages"
+		.popseg
+	.endif
+.endmacro
+
+; Expands the packed BCD into ASCII digits using a big lookup table.
+; Clobbers: A, X
+.macro mBcdToAsciiTable bcdOutput
+	.local @bcdScratchSize, @bcdScratch
+	@bcdScratchSize = (.sizeof(bcdOutput) + 1) / 2
+	@bcdScratch = bcdOutput + @bcdScratchSize - 1
+
+	.repeat .sizeof(bcdOutput)/2, i
+		lda @bcdScratch-i
+		tax
+
+		and #$0F
+		ora #'0'
+		sta bcdOutput+.sizeof(bcdOutput)-1-(i*2)
+
+		lda bcdAsciiTableHi, x
+		sta bcdOutput+.sizeof(bcdOutput)-1-(i*2)-1
+	.endrepeat
+	_mSetup_BcdToAsciiTableHi
+.endmacro
+
+; Expands the packed BCD into ASCII digits using two big lookup tables.
+; Clobbers: A, X
+.macro mBcdToAsciiTables bcdOutput
+	.local @bcdScratchSize, @bcdScratch
+	@bcdScratchSize = (.sizeof(bcdOutput) + 1) / 2
+	@bcdScratch = bcdOutput + @bcdScratchSize - 1
+
+	.repeat .sizeof(bcdOutput)/2, i
+		ldx @bcdScratch-i
+
+		lda bcdAsciiTableLo, x
+		sta bcdOutput+.sizeof(bcdOutput)-1-(i*2)
+
+		lda bcdAsciiTableHi, x
+		sta bcdOutput+.sizeof(bcdOutput)-1-(i*2)-1
+	.endrepeat
+	_mSetup_BcdToAsciiTableHi
+	.ifndef bcdAsciiTableLo
+		.pushseg
+		.segment "LIBDATA"
+		.align $100
+		bcdAsciiTableLo:
+			.repeat 10, h
+				.if h > 0
+					.res 16-10
+				.endif
+				.repeat 10, i
+					.byte '0'+i
+				.endrepeat
+			.endrepeat
+			; Crossing a page boundary will not cause failures, but
+			; will cause the code to run slower.
+			@ok = (.hibyte(*) = .hibyte(bcdAsciiTableLo))
+			.assert @ok, warning, "bcdAsciiTableLo crosses pages"
+		.popseg
+	.endif
+.endmacro
+
+.segment "LIBDATA"
+
+; This is a table of packed-BCD nibble pairs, where looking up a nibble
+; pair will return a new nibble pair with 3 added to nibbles >= 5. It
+; assumes valid input, that is, that each nibble is in the 0-9 range.
+bcdAdd3Table:
+	.repeat 10, high
+		.if high > 0
+			; Align the next elements to where they need to be.
+			.res 16-10
+		.endif
+		; Boolean expressions return 1 if true, 0 if false.
+		@high .set (high + (high >= 5) * 3) << 4
+		.repeat 10, low
+			.byte @high | (low + (low >= 5) * 3)
+		.endrepeat
+	.endrepeat
+
+; The table crossing a page boundary will not cause anything to fail,
+; but it will slow the algorithm down, so warn since we want max speed.
+@hi = .hibyte(bcdAdd3Table)
+.assert .hibyte(*) = @hi, warning, "bcdAdd3Table crosses page boundary"
